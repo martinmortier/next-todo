9662955aab59a4ab97b39dc0c78a4562f0283b1b
Mortier Martin <34869851+martinmortier@users.noreply.github.com>
2021-04-23 19:45:38 +0200

    Row data

--- components/PostGrid.jsx
+++ components/PostGrid.jsx
@@ -1,30 +1,36 @@
 import React from 'react'
 import { DataGrid, GridToolbar } from '@material-ui/data-grid';
+import Checkbox from '@material-ui/core/Checkbox';
+
 
 const PostGrid = ({listPost}) => {
     const columns = []
+    const rows = []
     const listsField = Object.keys(listPost[0])
+
+    const exchangeElem = () => {
+        let temp = listsField[2]
+        listsField[2] = listsField[3]
+        listsField[3] = temp
+    }
+    exchangeElem()
+
     listsField.map( lf => {
         columns.push({field: lf, headerName: lf.charAt(0).toUpperCase() + lf.slice(1), width:150})
     })
-
-      const rows = [
-        { id: 1, name: 'Snow', author: 'Jon' },
-        { id: 2, name: 'Lannister', author: 'Cersei' },
-        { id: 3, name: 'Lannister', author: 'Jaime' },
-        { id: 4, name: 'Stark', author: 'Arya' },
-        { id: 5, name: 'Targaryen', author: 'Daenerys' },
-        { id: 6, name: 'Melisandre', author: null },
-        { id: 7, name: 'Clifford', author: 'Ferrara' },
-        { id: 8, name: 'Frances', author: 'Rossini' },
-        { id: 9, name: 'Roxie', author: 'Harvey' },
-      ];
+    listPost.map(lp => {
+        rows.push({ id: lp.id, name: lp.name, done: <Checkbox defaultChecked />, author: lp.author})
+    })
+    
     return (
         <div style={{ height: 400, width: '50%' }}>
             <DataGrid columns={columns} rows={rows}
-  components={{
-    Toolbar: GridToolbar,
-  }}checkboxSelection/>
+                components={{
+                    Toolbar: GridToolbar,
+                }}
+                checkboxSelection
+                onSelectionModelChange={(newSelection) => console.log(newSelection.selectionModel)}
+            />
         </div>
     )
 }
